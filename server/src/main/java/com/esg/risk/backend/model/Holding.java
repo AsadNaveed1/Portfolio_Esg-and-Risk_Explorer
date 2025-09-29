d05d9ea2d9140e0711b3fef42a9c8c79662c7477
Asad <99832552+AsadNaveed1@users.noreply.github.com>
2025-09-29 22:18:09 +0800

    Frontend and Backend setup for the project

---
+++ server/src/main/java/com/esg/risk/backend/model/Holding.java
@@ -0,0 +1,28 @@
+package com.esg.risk.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Holding {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // Primary key
+
+    @ManyToOne
+    @JoinColumn(name = "portfolio_id")
+    private Portfolio portfolio;  // Links holding to portfolio
+
+    private String ticker;   // e.g. AAPL, TSLA
+    private Double weight;   // % of portfolio
+    private String sector;   // e.g. Tech, Energy
+    private String region;   // e.g. US, EU
+    private Double esgScore; // ESG score value
+
+
+}
---
+++ server/src/main/java/com/esg/risk/backend/model/Portfolio.java
@@ -0,0 +1,20 @@
+package com.esg.risk.backend.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private LocalDate uploadDate;
+    private String s3Path;
+}
